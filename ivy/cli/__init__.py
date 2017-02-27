6b2c5d60d816e3798fb29def4fc04b9a0bd61607
Darren Mulholland <darren@mulholland.xyz>
2017-02-27 14:48:04 +0000

    Add debug command to help text

--- ivy/cli/__init__.py
+++ ivy/cli/__init__.py
@@ -34,6 +34,7 @@ Flags:
 Commands:
   build               Build the site.
   clear               Clear the output directory.
+  debug               Print the node tree.
   init                Initialize a new site directory.
   serve               Run a web server on the site's output directory.
   watch               Monitor the site directory and rebuild on changes.
