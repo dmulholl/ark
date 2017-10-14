a7ca344dd149be4eae27d469d3dc989eafecc9c4
Darren Mulholland <darren@mulholland.xyz>
2017-10-14 11:06:17 +0100

    Housekeeping

--- ivy/ext/ivy_syntex.py
+++ ivy/ext/ivy_syntex.py
@@ -1,5 +1,7 @@
 # ------------------------------------------------------------------------------
-# This extension adds support for source files in Syntex format.
+# This extension adds support for source files in Syntex format. Syntex has
+# been renamed Monk - this file is retained for backwards compatability. It
+# will be removed in a future release.
 # ------------------------------------------------------------------------------
 
 import ivy
