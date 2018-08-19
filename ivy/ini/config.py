1cec5b4dcf6064fb78c10d0f69ce4a789788cd24
Darren Mulholland <darren@mulholland.xyz>
2018-08-19 20:39:36 +0100

    Housekeeping

--- ivy/ini/config.py
+++ ivy/ini/config.py
@@ -2,18 +2,18 @@
 # Site Configuration File
 # ------------------------------------------------------------------------------
 
-# This is a sample site configuration file. You can use this file to
-# customise how Ivy builds your site. Variables you set here will also be
-# available to themes and plugins. Note that you can safely delete this file
-# if your site does not require custom settings.
+# This is a sample site configuration file. You can use this file to customise
+# how Ivy builds your site. Variables you set here will also be available to
+# themes and plugins. Note that you can safely delete this file if your site
+# does not require custom settings.
 
 
 # ------------------------------------------------------------------------------
 # Theme
 # ------------------------------------------------------------------------------
 
-# Choose the theme to use when building your site. This variable should
-# specify the name of a theme directory in your site's 'lib' folder.
+# Choose the theme to use when building your site. This variable should specify
+# the name of a theme directory in your site's 'lib' folder.
 theme = "graphite"
 
 
@@ -25,17 +25,17 @@ theme = "graphite"
 title = "Site Title"
 
 # Site tagline - how this is displayed depends on the theme.
-tagline = "Just another Ivy site"
+tagline = "Just another Ivy site."
 
 
 # ------------------------------------------------------------------------------
 # Root URL
 # ------------------------------------------------------------------------------
 
-# Your root url can be an explicit domain ("http://example.com/") for
-# absolute urls, a single forward slash ("/") for site-relative urls, or an
-# empty string (the default) for page-relative urls. Unless you have a
-# specific reason for changing it, the default option is the most flexible.
+# Your root url can be an explicit domain ("http://example.com/") for absolute
+# urls, a single forward slash ("/") for site-relative urls, or an empty string
+# (the default) for page-relative urls. Unless you have a specific reason for
+# changing it, the default option is the most flexible.
 root = ""
 
 
@@ -43,6 +43,6 @@ root = ""
 # File Extension
 # ------------------------------------------------------------------------------
 
-# Choose a file extension for generated files, e.g. ".html" (the default),
-# or specify a forward slash ("/") to generate directory-style urls.
+# Choose a file extension for generated files, e.g. ".html" (the default), or
+# specify a forward slash ("/") to generate directory-style urls.
 extension = ".html"
