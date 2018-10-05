26e55d058c70cfdbef090834cd2861aee2d1cdbf
Darren Mulholland <darren@mulholland.xyz>
2018-10-05 14:42:43 +0100

    Housekeeping

--- ivy/ext/ivy_monk.py
+++ ivy/ext/ivy_monk.py
@@ -6,11 +6,13 @@ import ivy
 
 try:
     import monk
+except ImportError:
+    monk = None
 
-    @ivy.renderers.register('mk')
-    @ivy.renderers.register('monk')
+
+# The monk package is an optional dependency.
+if monk:
+
+    @ivy.renderers.register('mk', 'monk')
     def render(text):
         return monk.render(text, pygmentize=True)
-
-except ImportError:
-    pass
