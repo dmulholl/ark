167e2620c999f8e92ccbf4553fb3a7cefc1681aa
Darren Mulholland <dmulholl@tcd.ie>
2020-05-11 22:57:06 +0100

    Simplify the sample site configuration file

--- ivy/initsite/config.py
+++ ivy/initsite/config.py
@@ -6,42 +6,13 @@
 # how Ivy builds your site. Variables you set here will also be available to
 # themes and plugins.
 
-
-# ------------------------------------------------------------------------------
-# Theme
-# ------------------------------------------------------------------------------
-
 # Choose the theme to use when building your site. This variable should specify
 # the name of a theme directory in your site's 'lib' folder.
 theme = "graphite"
 
-
-# ------------------------------------------------------------------------------
-# Theme Settings
-# ------------------------------------------------------------------------------
-
-# Site title - how this is displayed depends on your theme.
+# Site title.
 title = "Site Title"
 
-# Site tagline - how this is displayed depends on your theme.
+# Site tagline.
 tagline = "Just another Ivy site."
 
-
-# ------------------------------------------------------------------------------
-# Root URL
-# ------------------------------------------------------------------------------
-
-# Your root url can be an explicit domain ("http://example.com/") for absolute
-# urls, a single forward slash ("/") for site-relative urls, or an empty string
-# (the default) for page-relative urls. Unless you have a specific reason for
-# changing it, the default option is the most flexible.
-root = ""
-
-
-# ------------------------------------------------------------------------------
-# File Extension
-# ------------------------------------------------------------------------------
-
-# Choose a file extension for generated files, e.g. ".html" (the default), or
-# specify a forward slash ("/") to generate directory-style urls.
-extension = ".html"
