e36ea43e8bc767805b8415f08fbde424e6e03bcc
Darren Mulholland <dmulholl@tcd.ie>
2020-07-08 20:01:01 +0100

    Enhance the 'tree' command
    Add support for displaying multiple attributes simultaneously.

--- ivy/ext/ivy_tree.py
+++ ivy/ext/ivy_tree.py
@@ -23,8 +23,12 @@ Usage: %s tree [FLAGS]
 
     $ ivy tree --attr title
 
+  Multiple attributes can be specified, e.g.
+
+    $ ivy tree --attr title --attr date
+
 Options:
-  -a, --attr <name>     Attribute to display.
+  -a, --attr <name>     Attribute(s) to display.
   -r, --root <url>      Specify the root node. (Defaults to the site root.)
 
 Flags:
@@ -54,18 +58,18 @@ def cmd_callback(cmd_name, cmd_parser):
         ivy.utils.termline()
         ivy.utils.safeprint('Site: %s' % ivy.site.home())
         ivy.utils.termline()
-        ivy.utils.safeprint(treestring(node, base=base, attr=cmd_parser.value('attr')))
+        ivy.utils.safeprint(treestring(node, depth=0, base=base, attrs=cmd_parser.values('attr')))
         ivy.utils.termline()
 
 
-def treestring(node, depth=0, base='url', attr=None):
+def treestring(node, depth, base, attrs):
     if base == 'url':
         line = '·  ' * depth + node.url
     else:
         line = '·  ' * depth + node.slug or '/'
-    if attr:
-        line += '  --  ' + repr(node.get(attr))
+    for attr in attrs:
+        line += '  \u001B[90m--\u001B[0m  ' + repr(node.get(attr))
     lines = [line]
     for child in node.children:
-        lines.append(treestring(child, depth + 1, base, attr))
+        lines.append(treestring(child, depth + 1, base, attrs))
     return '\n'.join(lines)
