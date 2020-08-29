b6f0973a12b9b58b32f6a7f4612f85f5f23c6709
Darren Mulholland <dmulholl@tcd.ie>
2020-08-29 10:53:40 +0100

    Bump dev version

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -2,7 +2,7 @@
 # Ivy: a static website generator.
 # ------------------------------------------------------------------------------
 
-__version__ = '3.0.0.dev5'
+__version__ = '3.0.0.dev6'
 
 import sys
 if sys.version_info < (3, 8):
