7b5f31d5fb1256e69c0ec5e2d5de565bb1287d8f
Darren Mulholland <dmulholl@tcd.ie>
2020-09-05 13:27:08 +0100

    Bump version to 3.0.0

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -2,7 +2,7 @@
 # Ivy: a static website generator.
 # ------------------------------------------------------------------------------
 
-__version__ = '3.0.0.rc1'
+__version__ = '3.0.0'
 
 import sys
 if sys.version_info < (3, 8):
