e33c243fe9b91d072d50a96d24bfaf1f85951389
Darren Mulholland <dmulholl@tcd.ie>
2020-09-05 12:18:15 +0100

    Update documentation

--- docs/config.py
+++ docs/config.py
@@ -9,4 +9,4 @@ Ivy is a static website generator built in Python. It transforms a directory of
 text files into a self-contained website.
 """
 
-version = "Version 3.0.0.dev6"
+version = "Version 3.0.0"
