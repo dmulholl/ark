94b4274d9b22b8a38ad4876cf84d085857bdb0d8
Darren Mulholland <dmulholl@tcd.ie>
2020-09-06 22:12:05 +0100

    Move all build callbacks to cli/build.py

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -2,7 +2,7 @@
 # Ivy: a static website generator.
 # ------------------------------------------------------------------------------
 
-__version__ = '3.0.0'
+__version__ = '3.1.0.dev'
 
 import sys
 if sys.version_info < (3, 8):
@@ -31,7 +31,6 @@ from . import nodes
 from . import pages
 from . import renderers
 from . import site
-from . import build
 from . import utils
 
 
--- ivy/cli/build.py
+++ ivy/cli/build.py
@@ -4,10 +4,14 @@
 
 import os
 import sys
+import datetime
 
 from .. import site
 from .. import events
 from .. import utils
+from .. import nodes
+from .. import pages
+from .. import filters
 
 
 helptext = """
@@ -31,8 +35,8 @@ Flags:
 
 
 @events.register('cli')
-def register_command(parser):
-    cmd = parser.command("build", helptext, cmd_callback)
+def register_command(argparser):
+    cmd = argparser.command("build", helptext, cmd_callback)
     cmd.flag("clear c")
     cmd.option("theme t")
 
@@ -51,3 +55,39 @@ def cmd_callback(cmd_name, cmd_parser):
         events.fire('main_build')
         events.fire('exit_build')
 
+
+@events.register('main_build')
+def build_site():
+
+    # Make sure we have a valid theme directory.
+    if not site.theme():
+        theme_name =  site.config['theme']
+        sys.exit(f"Error: cannot locate theme '{theme_name}'.")
+
+    # Copy the theme's resource files to the output directory.
+    if os.path.isdir(site.theme('resources')):
+        utils.copydir(site.theme('resources'), site.out())
+
+    # Copy the site's resource files to the output directory.
+    if os.path.exists(site.res()):
+        utils.copydir(site.res(), site.out())
+
+    # Callback to handle individual nodes.
+    def build_node(node):
+        if filters.apply('build_node', True, node):
+            node.render()
+            page = pages.Page(node)
+            page.write()
+
+    # Walk the node tree and pass each node to the handler.
+    nodes.root().walk(build_node)
+
+
+@events.register('exit_build')
+def print_build_stats():
+    report = datetime.datetime.now().strftime("[%H:%M:%S]")
+    report += f"   ·   Rendered: {site.rendered():5d}"
+    report += f"   ·   Written: {site.written():5d}"
+    report += f"   ·   Time: {site.runtime():6.2f} sec"
+    report = report.replace('·', '\u001B[90m·\u001B[0m')
+    utils.safeprint(report)
