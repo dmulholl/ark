384c14327431a2922f871e1bdbb85840f58dd7ba
Darren Mulholland <dmulholl@tcd.ie>
2021-07-13 14:24:19 +0100

    Graphite: tweak table styles

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -2,7 +2,7 @@
 # Ivy: a static website generator.
 # ------------------------------------------------------------------------------
 
-__version__ = '5.1.0'
+__version__ = '5.1.1'
 
 import sys
 if sys.version_info < (3, 8):
