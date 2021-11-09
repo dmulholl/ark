d748c2001d9c64e494a2b54cb55aea8c0405573d
Darren Mulholland <dmulholl@tcd.ie>
2021-11-09 12:54:46 +0000

    Housekeeping

--- ivy/ext/ivy_yaml.py
+++ ivy/ext/ivy_yaml.py
@@ -11,11 +11,11 @@ except ImportError:
     pass
 else:
     @ivy.filters.register('file_text')
-    def parse_yaml(raw_text, meta_dict):
-        if raw_text.startswith("---\n"):
-            if match := re.match(r"^---\n(.*?\n)---\n", raw_text, re.DOTALL):
-                filtered_text = raw_text[match.end(0):]
+    def parse_yaml(text, meta_dict):
+        if text.startswith("---\n"):
+            if match := re.match(r"^---\n(.*?\n)---\n", text, re.DOTALL):
+                text = text[match.end(0):]
                 data = yaml.safe_load(match.group(1))
                 if isinstance(data, dict):
                     meta_dict.update(data)
-        return filtered_text
+        return text
