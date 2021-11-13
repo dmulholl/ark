21359621656ffc6c8d1ef222a769fd4b0d045044
Darren Mulholland <dmulholl@tcd.ie>
2021-11-13 17:13:18 +0000

    Housekeeping

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -2,7 +2,7 @@
 # Ivy: a static website generator.
 # ------------------------------------------------------------------------------
 
-__version__ = '6.0.3'
+__version__ = '6.0.4'
 
 import sys
 if sys.version_info < (3, 8):
