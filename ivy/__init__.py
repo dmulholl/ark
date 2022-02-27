ee83abc03841921fd10fdeb1b532282544bc4b21
Darren Mulholland <dmulholl@tcd.ie>
2022-02-27 19:36:30 +0000

    Bump version to 6.2.0

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -2,7 +2,7 @@
 # Ivy: a static website generator.
 # ------------------------------------------------------------------------------
 
-__version__ = '6.2.0-beta'
+__version__ = '6.2.0'
 
 import sys
 if sys.version_info < (3, 8):
