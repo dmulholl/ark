3444fbf6f1d41f13cee03c906ff9331de6095d3f
Darren Mulholland <dmulholl@tcd.ie>
2022-03-27 13:28:24 +0100

    Housekeeping

--- ivy/ini/config.py
+++ ivy/ini/config.py
@@ -1,6 +1,9 @@
-# ------------------------------------------------------------------------------
-# Site Configuration File
-# ------------------------------------------------------------------------------
+# ------------------------- #
+#  Site Configuration File  #
+# ------------------------- #
+
+# Variables set here will be available in template files under a `site` attribute,
+# e.g. {{ site.title }}.
 
 # Choose the theme to use when building your site. This variable should specify
 # the name of a theme directory in your site's 'lib' folder.
